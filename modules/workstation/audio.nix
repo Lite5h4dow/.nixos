8df4311e53e48a2a89f691830c0aa999d95da385
Lamar Daughma <lite5h4dow+dev@gmail.com>
2024-11-12 22:22:02 +0000

    adding services

--- modules/workstation/audio.nix
+++ modules/workstation/audio.nix
@@ -1,6 +1,30 @@
 { lib, pkgs, ...}: {
-  services.pipewire.enable = lib.mkForce false;
+  # services.pipewire.enable = lib.mkForce false;
   hardware.pulseaudio.enable = true;
+
+  services = {
+    greetd = {
+      enable = true;
+      restart = true;
+      settings = {
+        default_session = {
+          command = "${lib.getExe pkgs.greetd.tuigreet} --window-padding 1 --time --time-format '%R - %F' --remember --remember-session --asterisks";
+          user = "greeter";
+        };
+      };
+    };
+
+    pipewire = {
+      enable = true;
+      wireplumber.enable = true;
+      pulse.enable = true;
+      alsa.enable = true;
+      jack.enable = true;
+    };
+    playerctld.enable = true;
+
+    
+  };
 
   environment.systemPackages = with pkgs;[
     pavucontrol
---
+++ modules/workstation/services.nix
@@ -0,0 +1,48 @@
+{
+  pkgs,
+  lib,
+  config,
+  ...
+}: {
+  systemd.user.services.polkit-authentication-agent = {
+    description = "PolicyKit Authentication Agent";
+    serviceConfig = {
+      Type = "simple";
+      ExecStart = "${pkgs.pantheon.pantheon-agent-polkit}/libexec/policykit-1-pantheon/io.elementary.desktop.agent-polkit";
+    };
+    wantedBy = ["graphical-session.target"];
+  };
+
+  xdg.portal = {
+    enable = true;
+    extraPortals = [pkgs.xdg-desktop-portal-gtk];
+    config.common.default = "gtk";
+  };
+
+  services = {
+    ananicy = {
+      enable = true;
+      package = pkgs.ananicy-cpp;
+      rulesProvider =
+        if config.programs.gamemode.enable
+        then
+          pkgs.ananicy-rules-cachyos.overrideAttrs {
+            preInstall = ''
+              rm -r 00-default/games
+            '';
+          }
+        else pkgs.ananicy-rules-cachyos;
+    };
+    power-profiles-daemon.enable = true;
+
+    # printing = {
+    #   enable = true;
+    #   drivers = [pkgs.canon-cups-ufr2];
+    # };
+    avahi = {
+      enable = true;
+      nssmdns4 = true;
+      openFirewall = true;
+    };
+  };
+}
