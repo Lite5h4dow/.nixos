33f97b741d3464450c2d291d0d679a4478beae5a
Lamar Daughma <lite5h4dow+dev@gmail.com>
2024-11-13 19:29:02 +0000

    adding some qt stuff

--- home/style/default.nix
+++ home/style/default.nix
@@ -7,7 +7,7 @@
   config = {
     stylix = {
       inherit (osConfig.stylix) icons;
-      # targets.qt = osConfig.stylix.targets.qt;
+      targets.qt = osConfig.stylix.targets.qt;
     };
   };
 }
