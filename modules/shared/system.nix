f9ebc88d4e05d34846a45b1ea2b32f2e05cd7eeb
Lamar Daughma <lite5h4dow+dev@gmail.com>
2024-11-13 23:31:22 +0000

    adding editor as helix

--- modules/shared/system.nix
+++ modules/shared/system.nix
@@ -3,4 +3,9 @@
   i18n.defaultLocale = "en_US.UTF-8";
   # console.keymap = "us";
   # networking.nameservers = [1.1.1.1, 1.0.0.1, 8.8.8.8, 8.8.4.4];
+  environment = {
+    variables = {
+      EDITOR = "hx";
+    };
+  };
 }
