b4ca8d338a23874f00a2913a55881ca708e6da08
Lamar Daughma <lite5h4dow+dev@gmail.com>
2024-11-13 14:14:29 +0000

    adding extenstions

--- home/schizofox.nix
+++ home/schizofox.nix
@@ -5,7 +5,12 @@
     enable = true;
 
     extensions = {
-      bitwarden.enable = true;
+      simplefox.enable = true;
+      darkreader.enable = true;
+
+      extraExtensions = {
+        "webextension@bitwarden.com".install_url = "https://addons.mozilla.org/firefox/downloads/file/4371752/bitwarden_password_manager-2024.10.1.xpi";
+      };
     };
   };
 }
