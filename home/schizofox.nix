245742185e5b4c0128d9bdfeff6c22f11b2c3945
Lamar Daughma <lite5h4dow+dev@gmail.com>
2024-11-13 14:28:35 +0000

    adding bitwarden for real this time

--- home/schizofox.nix
+++ home/schizofox.nix
@@ -8,6 +8,11 @@
       # simplefox.enable = true;
       # darkreader.enable = true;
       # bitwarden.enable = true;
+      extraExtensions = let
+        mkUrl = name: "https://addons.mozilla.org/firefox/downloads/latest/${name}/latest.xpi";
+      in {
+        "{446900e4-71c2-419f-a6a7-df9c091e268b}".install_url = mkUrl "bitwarden-password-manager";
+      };
     };
   };
 }
