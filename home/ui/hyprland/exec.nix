995fbb77805d4c24e5f2cd9b50914277139783f6
Lamar Daughma <lite5h4dow+dev@gmail.com>
2024-11-16 13:17:23 +0000

    ags v2 command

--- home/ui/hyprland/exec.nix
+++ home/ui/hyprland/exec.nix
@@ -8,7 +8,7 @@
       [
         "[workspace 1] firefox"
         "gammastep -l 47.1625:19.5033 -t 6500K:2600K"
-        "ags"
+        "ags run"
       ]
       ++ lib.optional (osConfig.values.terminal.firstInstance != null) "[workspace special:terminal silent] ${osConfig.values.terminal.firstInstance}";
   };
