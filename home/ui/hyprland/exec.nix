8ffee54f3b98e25f3849c25c3b7d8bbd95b15ea9
Lamar Daughma <lite5h4dow+dev@gmail.com>
2024-11-16 13:20:33 +0000

    adding ags first and immediately forking to background

--- home/ui/hyprland/exec.nix
+++ home/ui/hyprland/exec.nix
@@ -6,9 +6,9 @@
   wayland.windowManager.hyprland.settings = {
     exec-once =
       [
+        "ags run &"
         "[workspace 1] firefox"
-        "gammastep -l 47.1625:19.5033 -t 6500K:2600K"
-        "ags run"
+        "gammastep -l 47.1625:19.5033 -t 6500K:2600K &"
       ]
       ++ lib.optional (osConfig.values.terminal.firstInstance != null) "[workspace special:terminal silent] ${osConfig.values.terminal.firstInstance}";
   };
