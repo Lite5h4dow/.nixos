81e5a4e34a377feade572c4527a53a1ad6269b04
Lamar Daughma <lite5h4dow+dev@gmail.com>
2024-11-17 12:40:07 +0000

    pushing local changes

--- home/style/shared.nix
+++ home/style/shared.nix
@@ -1,9 +1,10 @@
 { config, ... }:let
 inherit (config.lib.stylix) colors;
+inherit (builtins) toJSON readFile;
 in {
   home.file = {
     "./.config/shared/theme.test" = {
-      text = builtins.toJSON(colors.withHashtag);
+      text = toJSON(readFile(colors.withHashtag));
       executable = false;
     };
   };
