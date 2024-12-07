980defb29921e9a711f9fd096bbb0e9ff687adf9
litelotus <lite5h4dow+dev@gmail.com>
2024-12-07 01:14:49 +0000

    adding prisma stuff

--- home/dev/nodejs.nix
+++ home/dev/nodejs.nix
@@ -9,5 +9,14 @@
     sass
     scss-lint
     nodePackages_latest.nodemon
+    nodePackages_latest.prisma
   ];
+
+  home.sessionVariables = with pkgs; {
+    PRISMA_MIGRATION_ENGINE_BINARY = "${prisma-engines}/bin/schema-engine";
+    PRISMA_QUERY_ENGINE_BINARY ="${prisma-engines}/bin/query-engine";
+    PRISMA_QUERY_ENGINE_LIBRARY="${prisma-engines}/lib/libquery_engine.node";
+    PRISMA_INTROSPECTION_ENGINE_BINARY="${prisma-engines}/bin/introspection-engine";
+    PRISMA_FMT_BINARY="${prisma-engines}/bin/prisma-fmt";
+  };
 }
