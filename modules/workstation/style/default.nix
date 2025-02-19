e81fc41a1d73402525c008e576c57ce0d081b659
litelotus <lite5h4dow+dev@gmail.com>
2025-02-19 20:40:24 +0000

    removing references to wallpaper configs

--- modules/workstation/style/default.nix
+++ modules/workstation/style/default.nix
@@ -15,7 +15,7 @@
       enable = true;
       base16Scheme = pkgs.base16-schemes + /share/themes/catppuccin-mocha.yaml;
       polarity = "dark";
-      image = inputs.wallpapers + /a-collection-of-autumn-vibes-21x9-3440x1440-wallpapers-v0-j66d7h4c9wzd1.webp;
+      # image = inputs.wallpapers + /a-collection-of-autumn-vibes-21x9-3440x1440-wallpapers-v0-j66d7h4c9wzd1.webp;
     };
   };
 }
