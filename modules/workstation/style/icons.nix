2b9a43ed6f48ed851a97bc89fbde3f8f2b8c968e
litelotus <lite5h4dow+dev@gmail.com>
2025-03-01 00:46:48 +0000

    updating

--- modules/workstation/style/icons.nix
+++ modules/workstation/style/icons.nix
@@ -13,7 +13,8 @@
 
   environment.systemPackages = [
     config.stylix.icons.package
-    pkgs.breeze-icons
+    pkgs.libsForQt5.breeze-icons
+    # pkgs.breeze-icons
   ];
   programs.gdk-pixbuf.modulePackages = [pkgs.librsvg];
 }
