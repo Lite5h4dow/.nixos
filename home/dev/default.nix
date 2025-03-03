5dc34d318be855d2f42db851e02292aa6584ea81
litelotus <lite5h4dow+dev@gmail.com>
2025-03-03 20:39:01 +0000

    adding gcloud auth to podman

--- home/dev/default.nix
+++ home/dev/default.nix
@@ -1,22 +1,86 @@
-{pkgs, ...}:{
+{lib, pkgs, osConfig, ...}:let
+  inherit (osConfig.custom) podman;
+  inherit (lib) optional;
+in{
   imports = [
     ./git.nix
     ./nodejs.nix
   ];
 
-  home.packages = with pkgs; [
-    go
-    nixd
-    android-tools
-    dotnet-sdk
-    fractal
-    dotnetPackages.Nuget
-    kubectl
-    woodpecker-cli
-    gnumake
-    python3
-    usbutils
-    gcc-arm-embedded
-    dfu-util
-  ];
+  home={
+    packages = with pkgs; [
+      go
+      nixd
+      android-tools
+      dotnet-sdk
+      fractal
+      dotnetPackages.Nuget
+      kubectl
+      woodpecker-cli
+      gnumake
+      python3
+      usbutils
+      gcc-arm-embedded
+      dfu-util
+    ];
+
+    file = {
+      ".config/containers/config.json" = optional podman.enable {
+        text = ''
+          {
+            "credHelpers": {
+              "gcr.io": "gcloud",
+              "us.gcr.io": "gcloud",
+              "eu.gcr.io": "gcloud",
+              "asia.gcr.io": "gcloud",
+              "staging-k8s.gcr.io": "gcloud",
+              "marketplace.gcr.io": "gcloud",
+              "asia-docker.pkg.dev":  "gcloud",
+              "asia-east1-docker.pkg.dev": "gcloud",
+              "asia-east2-docker.pkg.dev":  "gcloud",
+              "asia-northeast1-docker.pkg.dev": "gcloud",
+              "asia-northeast2-docker.pkg.dev":  "gcloud",
+              "asia-northeast3-docker.pkg.dev": "gcloud",
+              "asia-south1-docker.pkg.dev":  "gcloud",
+              "asia-south2-docker.pkg.dev": "gcloud",
+              "asia-southeast1-docker.pkg.dev":  "gcloud",
+              "asia-southeast2-docker.pkg.dev": "gcloud",
+              "australia-southeast1-docker.pkg.dev":  "gcloud",
+              "australia-southeast2-docker.pkg.dev": "gcloud",
+              "europe-docker.pkg.dev":  "gcloud",
+              "europe-central2-docker.pkg.dev": "gcloud",
+              "europe-north1-docker.pkg.dev":  "gcloud",
+              "europe-southwest1-docker.pkg.dev": "gcloud",
+              "europe-west1-docker.pkg.dev":  "gcloud",
+              "europe-west10-docker.pkg.dev": "gcloud",
+              "europe-west12-docker.pkg.dev":  "gcloud",
+              "europe-west2-docker.pkg.dev": "gcloud",
+              "europe-west3-docker.pkg.dev":  "gcloud",
+              "europe-west4-docker.pkg.dev": "gcloud",
+              "europe-west6-docker.pkg.dev":  "gcloud",
+              "europe-west8-docker.pkg.dev": "gcloud",
+              "europe-west9-docker.pkg.dev":  "gcloud",
+              "me-central1-docker.pkg.dev": "gcloud",
+              "me-central2-docker.pkg.dev":  "gcloud",
+              "me-west1-docker.pkg.dev": "gcloud",
+              "northamerica-northeast1-docker.pkg.dev":  "gcloud",
+              "northamerica-northeast2-docker.pkg.dev": "gcloud",
+              "southamerica-east1-docker.pkg.dev":  "gcloud",
+              "southamerica-west1-docker.pkg.dev": "gcloud",
+              "us-docker.pkg.dev":  "gcloud",
+              "us-central1-docker.pkg.dev": "gcloud",
+              "us-east1-docker.pkg.dev":  "gcloud",
+              "us-east4-docker.pkg.dev": "gcloud",
+              "us-east5-docker.pkg.dev":  "gcloud",
+              "us-south1-docker.pkg.dev": "gcloud",
+              "us-west1-docker.pkg.dev":  "gcloud",
+              "us-west2-docker.pkg.dev": "gcloud",
+              "us-west3-docker.pkg.dev":  "gcloud",
+              "us-west4-docker.pkg.dev": "gcloud"
+            }
+          }
+        '';
+      };
+    };
+  };
 }
