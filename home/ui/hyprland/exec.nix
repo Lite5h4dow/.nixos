40c86310c4a7baa99b976e1577c937cfe9891afc
litelotus <lite5h4dow+dev@gmail.com>
2025-03-03 21:30:18 +0000

    switch hyprland browser

--- home/ui/hyprland/exec.nix
+++ home/ui/hyprland/exec.nix
@@ -7,7 +7,7 @@
     exec-once =
       [
         "ags run &"
-        "[workspace 1] firefox"
+        "[workspace 1] zen"
         "gammastep -l 47.1625:19.5033 -t 6500K:2600K &"
       ]
       ++ lib.optional (osConfig.values.terminal.firstInstance != null) "[workspace special:terminal silent] ${osConfig.values.terminal.firstInstance}";
