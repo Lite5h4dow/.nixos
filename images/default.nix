431ae57bda1055a84bf3908d7007e6dd97acdc89
litelotus <lite5h4dow+dev@gmail.com>
2025-03-18 23:49:46 +0000

    adding unique images

--- images/default.nix
+++ images/default.nix
@@ -11,16 +11,33 @@
   raspberry-pi = modulePath + /raspberry-pi;
   raspberry-pi-host = hostPath + /raspberry-pi;
 
-  pi-image = mkNixosSystem {
+  pi-modules = [
+    "${nixpkgs}/nixos/modules/installer/sd-card/sd-image-aarch64.nix"
+    raspberry-pi-host
+    raspberry-pi
+    shared
+  ];
+
+  pi-02w = mkNixosSystem {
     system = "aarch64-linux";
-    modules = [
-      "${nixpkgs}/nixos/modules/installer/sd-card/sd-image-aarch64.nix"
-      raspberry-pi-host
-      raspberry-pi
-      shared
-    ];
+    modules = pi-modules
+      ++ raspberry-pi-host + /rpi02w.nix;
+  };
+
+  pi-0 = mkNixosSystem {
+    system = "aarch64-linux";
+    modules = pi-modules
+      ++ raspberry-pi-host + /rpi0.nix;
+  };
+
+  pi-3 = mkNixosSystem {
+    system = "aarch64-linux";
+    modules = pi-modules
+      ++ raspberry-pi-host + /rpi3.nix;
   };
 
 in {
-  raspberry-pi = pi-image.config.system.build.sdImage;
+  rpi02w = pi-02w.config.system.build.sdImage;
+  rpi0 = pi-0.config.system.build.sdImage;
+  rpi3 = pi-3.config.system.build.sdImage;
   }
