3c47995f316c42b7c60efc36cb803bfcb2d6e635
litelotus <lite5h4dow+dev@gmail.com>
2025-03-18 16:50:58 +0000

    fixing

--- images/default.nix
+++ images/default.nix
@@ -3,17 +3,19 @@
   inherit (lib') mkNixosSystem;
 
   modulePath = ../modules;
+  hostPath = ../hosts;
 
   shared = modulePath + /shared;
   workstation = modulePath + /workstation;
   server = modulePath + /server;
   raspberry-pi = modulePath + /raspberry-pi;
+  raspberry-pi-host = hostPath + /raspberry-pi;
 
   pi-image = mkNixosSystem {
     system = "aarch64-linux";
     modules = [
       "${nixpkgs}/nixos/modules/installer/sd-card/sd-image-aarch64.nix"
-      ./raspberry-pi
+      raspberry-pi-host
       raspberry-pi
       shared
     ];
