b015e182b60ab9fd25042c32453e6a1caa857e51
litelotus <lite5h4dow+dev@gmail.com>
2025-03-19 00:15:10 +0000

    adding lambda

--- images/default.nix
+++ images/default.nix
@@ -1,6 +1,7 @@
-{lib', inputs}: let
+{lib', lib, inputs}: let
   inherit (inputs) nixpkgs;
   inherit (lib') mkNixosSystem;
+  inherit (lib) optional;
 
   modulePath = ../modules;
   hostPath = ../hosts;
@@ -11,33 +12,34 @@
   raspberry-pi = modulePath + /raspberry-pi;
   raspberry-pi-host = hostPath + /raspberry-pi;
 
-  pi-modules = [
+  pi-modules = os: [
     "${nixpkgs}/nixos/modules/installer/sd-card/sd-image-aarch64.nix"
     raspberry-pi-host
     raspberry-pi
     shared
-  ];
+  ]
+  ++ optional (os == "rpi0") (raspberry-pi-host + /rpi0.nix)
+  ++ optional (os == "rpi02w") (raspberry-pi-host + /rpi02w.nix)
+  ++ optional (os == "rpi3") (raspberry-pi-host + /rpi3.nix)
+  ;
 
-  pi-02w = mkNixosSystem {
+  "rpi02w" = mkNixosSystem {
     system = "aarch64-linux";
-    modules = pi-modules
-      ++ raspberry-pi-host + /rpi02w.nix;
+    modules = pi-modules "rpi02w";
   };
 
-  pi-0 = mkNixosSystem {
+  "rpi0" = mkNixosSystem {
     system = "aarch64-linux";
-    modules = pi-modules
-      ++ raspberry-pi-host + /rpi0.nix;
+    modules = pi-modules "rpi0";
   };
 
-  pi-3 = mkNixosSystem {
+  "rpi3" = mkNixosSystem {
     system = "aarch64-linux";
-    modules = pi-modules
-      ++ raspberry-pi-host + /rpi3.nix;
+    modules = pi-modules "rpi3";
   };
 
 in {
-  rpi02w = pi-02w.config.system.build.sdImage;
-  rpi0 = pi-0.config.system.build.sdImage;
-  rpi3 = pi-3.config.system.build.sdImage;
+  rpi02w = rpi02w.config.system.build.sdImage;
+  rpi0 = rpi0.config.system.build.sdImage;
+  rpi3 = rpi3.config.system.build.sdImage;
   }
