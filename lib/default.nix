6cb9bcab210fbff7e6f5d5e0ea8842f8de69d93f
litelotus <lite5h4dow+dev@gmail.com>
2025-03-28 00:03:06 +0000

    i dont know what im doing here

--- lib/default.nix
+++ lib/default.nix
@@ -38,7 +38,7 @@
 
   mkNixOnDroidSystem = args @ {system, nixpkgs ? inputs.nixpkgs, ...}:
     inputs.nix-on-droid.lib.nixOnDroidConfiguration {
-      pkgs = nixpkgs.legacyPackages {system = system;};
+      pkgs = nixpkgs;
       system = null;
 
       modules = [
