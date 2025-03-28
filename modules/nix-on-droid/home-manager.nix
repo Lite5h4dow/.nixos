3dcbeaad6565af10f59775329e5e28f5f6276358
litelotus <lite5h4dow+dev@gmail.com>
2025-03-28 00:27:03 +0000

    adding missing nixondroid files

---
+++ modules/nix-on-droid/home-manager.nix
@@ -0,0 +1,20 @@
+{ inputs, system, lib', config, flakePkgs, customPkgs, ... }: let
+  username = config.values.mainUser;
+in {
+  imports = [ inputs.home-manager.nixosModules.home-manager];
+
+  config = {
+    users.users.${username}.initialPassword = "${username}";
+    home-manager = {
+      verbose = true;
+      useGlobalPkgs = true;
+      useUserPackages = true;
+      backupFileExtension = "old";
+      extraSpecialArgs = {
+        inherit inputs system flakePkgs customPkgs lib';
+      };
+      
+      users.${username} = ../../home/wsl.nix;
+    };
+  };
+}
