a53c967707b3dad58574587061aae25f6ec5b370
litelotus <lite5h4dow+dev@gmail.com>
2025-04-15 17:37:53 +0900

    adding missing packages from nixondroidconfig

--- lib/default.nix
+++ lib/default.nix
@@ -38,6 +38,7 @@
 
   mkNixOnDroidSystem = args @ {system, nixpkgs ? inputs.nixpkgs, ...}:
     inputs.nix-on-droid.lib.nixOnDroidConfiguration {
+      pkgs = nixpkgs {system = system;};
       modules = [
         {
           _module.args = {
