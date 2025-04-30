38ce079e48f51ddd74565134dc12dd4c63dc6365
litelotus <lite5h4dow+dev@gmail.com>
2025-04-30 17:22:41 +0900

    stop building hyprland

--- modules/shared/nix.nix
+++ modules/shared/nix.nix
@@ -26,12 +26,12 @@ in {
 
       substituters=[
         "https://cache.nixos.org/"
-      ];
-      extra-substituters = [
         "https://nix-community.cachix.org"
+        "https://hyprland.cachix.org"
       ];
-      extra-trusted-public-keys = [
+      trusted-public-keys = [
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+        "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
       ];
     };
 
