a796c9b07b199afacd4797ba50990d7fb4290e87
litelotus <lite5h4dow+dev@gmail.com>
2025-04-30 17:34:13 +0900

    adding cachix that should make sense

--- modules/shared/nix.nix
+++ modules/shared/nix.nix
@@ -26,10 +26,14 @@ in {
 
       substituters=[
         "https://cache.nixos.org/"
-        "https://nix-community.cachix.org"
+      ];
+      extra-substituters=[
         "https://hyprland.cachix.org"
+        "https://nix-community.cachix.org"
       ];
       trusted-public-keys = [
+      ];
+      extra-trusted-public-keys = [
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
         "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
       ];
