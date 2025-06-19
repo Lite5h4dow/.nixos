5775758d56475aef66d7945a04bb290f9f819135
litelotus <lite5h4dow+dev@gmail.com>
2025-06-19 12:34:46 +0000

    adding kubernetes port to firewall

--- hosts/server/01/default.nix
+++ hosts/server/01/default.nix
@@ -21,12 +21,14 @@
         22
         80
         443
+        6443
         25565
       ];
       allowedUDPPorts =[
         25565
       ];
     };
+
     defaultGateway= "192.168.1.1";
     nameservers = ["1.1.1.1"];
     hostName = "01.server";
