fadb1c44d6f91614fe414a70a6848d1dbc716e8a
litelotus <lite5h4dow+dev@gmail.com>
2025-06-21 18:27:34 +0000

    hmmmmm

--- modules/server/kubernetes.nix
+++ modules/server/kubernetes.nix
@@ -54,7 +54,7 @@ in{
         advertiseAddress = kubeMasterIP;
       };
 
-      easyCerts = true;
+      # easyCerts = true;
       addons.dns.enable = true;
       kubelet.extraOpts = "--fail-swap-on=false";
     };
