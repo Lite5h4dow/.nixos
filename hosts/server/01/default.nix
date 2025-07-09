d3621a33e853bf5420ffe0b0f9e5b63a2113025d
litelotus <lite5h4dow+dev@gmail.com>
2025-07-09 10:26:20 +0000

    adding k3s changes

--- hosts/server/01/default.nix
+++ hosts/server/01/default.nix
@@ -7,7 +7,8 @@
   services ={
     k3s ={
       enable = true;
-      token = "701a0bcbd15a1fa49b45ea342a3c216f"; #need to find a way to pull this in
+      # token = "701a0bcbd15a1fa49b45ea342a3c216f"; #need to find a way to pull this in
+      token = "K107a1c725b53d9004555c0b543328b06434c33c6199a21bc1ef6b10f898669a897::server:701a0bcbd15a1fa49b45ea342a3c216f"; #need to find away to pull this in
       role = "server";
       clusterInit = true;
     };
@@ -23,12 +24,14 @@
         443
         2202
         2379
+        2380
         6443
         8888
         25565
       ];
       allowedUDPPorts =[
         25565
+        8472
       ];
     };
 
