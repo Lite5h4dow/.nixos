601a2356dbd1ebde4cf7b31b21a578dda61dcecf
litelotus <lite5h4dow+dev@gmail.com>
2025-07-11 10:21:03 +0100

    pushing server changes

--- home/ui/ghostty/default.nix
+++ home/ui/ghostty/default.nix
@@ -1,23 +1,21 @@
 { config, ...}:let
   inherit (config.stylix) fonts;
 in {
-  home={
-    programs.ghostty = {
-      enable = true;
-      enableFishIntegration = true;
-      installBatSyntax = true;
-      installVimSyntax = true;
+  programs.ghostty = {
+    enable = true;
+    enableFishIntegration = true;
+    installBatSyntax = true;
+    installVimSyntax = true;
 
-      config = {
-        font-family = fonts.sansSerif.name;
-        theme = "catppuccin-mocha";
-      };
-    };
-
-    file = {
-      ".config/ghostty/config" ={
-        source = ./config;
-      };
-    };
+    # config = {
+    #   font-family = fonts.sansSerif.name;
+    #   theme = "catppuccin-mocha";
+    # };
   };
+
+  #   file = {
+  #   ".config/ghostty/config" ={
+  #     source = ./config;
+  #   };
+  # };
 }
