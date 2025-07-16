84604ca9c09acb49799dbf1f244d17dc5b5cda68
litelotus <lite5h4dow+dev@gmail.com>
2025-07-16 13:30:23 +0100

    back to mkderivation

--- hosts/server/default.nix
+++ hosts/server/default.nix
@@ -1,16 +1,20 @@
 { config, lib, pkgs, system, ...}: let
   inherit (config) values;
   inherit (lib) mkForce;
+  inherit (pkgs.stdenv) mkDerivation;
   
   rke2-traefik-config = ''
     ingress-controller: traefik
   '';
   
-  rke2-traefik = derivation {
+  rke2-traefik = mkDerivation {
     name = "rke2-use-traefik";
-    system = system;
-    builder = "${pkgs.bash}/bin/bash";
-    args = ["-c" "echo" "${rke2-traefik-config} > $out/etc/rancher/rke2/config.yaml.d/00-use-traefik.yaml"];
+    dontUnpack = true;
+    buildCommand = "echo ${rke2-traefik-config} > $out";
+    installPhase = ''
+    mkdir -p /etc/rancher/rke2/config.yaml.d
+    cp $out /etc/rancher/rke2/config.yaml.d/00-use-traefik.yaml
+    '';
   };
 in {
   zramSwap.enable = true;
