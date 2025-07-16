27b0cc771cffa5770b528ae4e88270b0c2ae98e0
litelotus <lite5h4dow+dev@gmail.com>
2025-07-16 12:58:54 +0100

    adding a config derivation test

--- hosts/server/default.nix
+++ hosts/server/default.nix
@@ -1,6 +1,16 @@
 { config, lib, pkgs, ...}: let
   inherit (config) values;
   inherit (lib) mkForce;
+  inherit (pkgs.stdenv) mkDerivation;
+  
+  rke2-traefik-config = ''
+    ingress-controller: traefik
+  '';
+  
+  rke2-traefik = mkDerivation {
+    name = "rke2 use traefik";
+    installPhase = "echo ${rke2-traefik-config} > /etc/rancher/rke2/config.yaml.d/00-use-traefik.yaml";
+  };
 in {
   zramSwap.enable = true;
 
@@ -19,4 +29,7 @@ in {
       };
     };
   };
+
+  environment.systemPackages = [ rke2-traefik ];
+
 }
