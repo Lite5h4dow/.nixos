9ddf91b372fa8f26144b47a814e66e61d56423de
litelotus <lite5h4dow+dev@gmail.com>
2025-07-16 13:14:58 +0100

    adding system

--- hosts/server/default.nix
+++ hosts/server/default.nix
@@ -1,15 +1,16 @@
-{ config, lib, pkgs, ...}: let
+{ config, lib, pkgs, system, ...}: let
   inherit (config) values;
   inherit (lib) mkForce;
-  inherit (pkgs.stdenv) mkDerivation;
   
   rke2-traefik-config = ''
     ingress-controller: traefik
   '';
   
-  rke2-traefik = mkDerivation {
+  rke2-traefik = derivation {
     name = "rke2 use traefik";
-    installPhase = "echo ${rke2-traefik-config} > /etc/rancher/rke2/config.yaml.d/00-use-traefik.yaml";
+    system = system;
+    builder = "${pkgs.bash}/bin/bash";
+    args = ["-c" "echo" "${rke2-traefik-config} > $out/etc/rancher/rke2/config.yaml.d/00-use-traefik.yaml"];
   };
 in {
   zramSwap.enable = true;
