7ac446eb50c3d17e5d14922765560e696978ef08
litelotus <lite5h4dow+dev@gmail.com>
2025-09-18 14:10:11 +0100

    adding dts file

--- hosts/raspberry-pi/prodpod.nix
+++ hosts/raspberry-pi/prodpod.nix
@@ -1,3 +1,13 @@
 {pkgs, ...}:{
   boot.kernelPackages = pkgs.linuxPackages_latest;
+   hardware.deviceTree = {
+     enable = true;
+     overlays = [
+      {
+        name = "prodpod-device";
+        dtsFile = ./prodpod.dts;
+      }
+    ]
+    ;
+   };
 }
