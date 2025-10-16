9b5f8a2c4fcf4b459e106441d5e6c6b48a9d883b
litelotus <lite5h4dow+dev@gmail.com>
2025-10-16 21:54:12 +0100

    adding custom uboot

--- hosts/radxa/prodpod.nix
+++ hosts/radxa/prodpod.nix
@@ -1,4 +1,29 @@
-{inputs, pkgs,...}:{
+{ inputs, pkgs, ... }:
+let
+  cm3j-uboot = pkgs.buildUBoot {
+    src = fetchGit { url = "https://git.litelot.us/litelotus/u-boot.git"; };
+    defConfig = "radxa-cm3j-rpi-cm4-io-rk3568_defconfig";
+    extraMeta.platforms = [ "aarch64-linux" ];
+    filesToInstall = [
+      "idbloader.img"
+      "u-boot.itb"
+      "u-boot-rockchip.bin"
+    ];
+  };
+
+in
+{
   custom.rebuildName = "prodpod";
-  # boot.kernelPackages = pkgs.linuxPackages_latest;
+
+  environment.systemPackages = [
+    cm3j-uboot
+  ];
+
+  boot = {
+    loader = {
+      grub.enable = false;
+      generic-extlinux-compatible = true;
+    };
+  };
+
 }
