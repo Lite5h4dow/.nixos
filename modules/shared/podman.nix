d682cb8d569010831aecd71ea884910ad6e4c8a4
litelotus <lite5h4dow+dev@gmail.com>
2025-11-12 11:07:21 +0000

    trying

--- modules/shared/podman.nix
+++ modules/shared/podman.nix
@@ -1,11 +1,23 @@
-{config, pkgs, lib, ...}: let
+{
+  config,
+  pkgs,
+  lib,
+  ...
+}:
+let
   inherit (config.values) mainUser;
   inherit (config.custom) graphics docker podman;
   inherit (graphics) nvidia;
-  inherit (lib) types mkEnableOption mkOption mkIf optionalAttrs;
-in {
+  inherit (lib)
+    types
+    mkEnableOption
+    mkOption
+    mkIf
+    ;
+in
+{
   options = {
-    custom.podman ={
+    custom.podman = {
       enable = mkEnableOption {
         description = "Enable Podman module";
         type = types.bool;
@@ -25,34 +37,32 @@ in {
     };
   };
 
-  config = mkIf podman.enable (let
-     podman-wsl' = pkgs.symlinkJoin {
-      name = "podman";
-      paths = [pkgs.podman];
-      nativeBuildInputs = [pkgs.makeBinaryWrapper];
-      postBuild = ''
-        wrapProgram $out/bin/podman --add-flags "-r"
-      '';
-     };
-    in{
+  config = mkIf podman.enable ({
     virtualisation = {
       podman = {
         enable = true;
-        package = if podman.wsl then podman-wsl' else pkgs.podman;
+        package =
+          if podman.wsl then
+            pkgs.podman.overrideAttrs (oldAttrs: {
+              buildInputs = oldAttrs.buildInputs or [ ] ++ [ pkgs.makeWrapper ];
+              postInstall = oldAttrs.postInstall or "" + ''
+                wrapProgram $out/bin/podman --add-flags "-r"
+              '';
+            })
+          else
+            pkgs.podman;
         dockerCompat = !docker.enable;
         dockerSocket.enable = !docker.enable;
         autoPrune.enable = true;
       };
-
-
       oci-containers.backend = "podman";
     };
 
-    environment.systemPackages = with pkgs;[
+    environment.systemPackages = with pkgs; [
       podman-compose
     ];
 
-    users.users.${mainUser}.extraGroups = ["podman"];
+    users.users.${mainUser}.extraGroups = [ "podman" ];
 
     hardware.nvidia-container-toolkit.enable = podman.nvidia;
   });
